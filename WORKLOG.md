3a1c0f6127e63ee2615987f9535208682e078ec7
Salaj Rijal <srijal30@stuy.edu>
2023-05-23 23:07:22 -0400

    created templates + plan

--- WORKLOG.md
+++ WORKLOG.md
@@ -25,7 +25,7 @@
 - was absent (AP Exam)
 
 ### 05/22/23
-- was absent
+- was absent (sick)
 - read/watched these resources
 	- https://www.youtube.com/watch?v=YVlTDPhTA9U
 	- https://www.youtube.com/watch?v=hJ8IwyhqzD4
@@ -40,3 +40,4 @@
 - got 32 bit buffer overflow attack to work
 - https://medium.com/@buff3r/basic-buffer-overflow-on-64-bit-architecture-3fb74bab3558
 - outlined features that our app will have
+- created template for tool src files
---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+
+// ./tool --help
+
+// ./tool override-return <prgm> <offset> <new-return-address>
+
+// ./tool find-offset <prgm>
+
+// ./tool list-functions <prgm>
+
+// ./tool list-strings <prgm>
+
+
+// IF WE HAVE TIME
+// these attacks are for either running your own script or modifying the program values by knowing source code
+
+// ./tool custom-payload <prgm> 
+
+
+
+}
