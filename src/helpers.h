265bde2b69ad3238af6f96fc0c70439f18023594
Salaj Rijal <srijal30@stuy.edu>
2023-06-01 19:06:37 -0400

    debug

--- src/helpers.h
+++ src/helpers.h
@@ -3,6 +3,6 @@
 
 void get_functions(char* prgrm);
 void find_offset(char* prgrm);
-void override_return(char* prgrm, char* newRet, int offset);
+void override_return(char* prgrm, int newRet, int offset);
 
 #endif
