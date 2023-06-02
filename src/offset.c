000147712222b331eef691a4a90c03eb2f4baaed
Salaj Rijal <srijal30@stuy.edu>
2023-06-01 23:55:32 -0400

    worked on offset

--- src/offset.c
+++ src/offset.c
@@ -1,10 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+#include <sys/wait.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/user.h>
 
+void 
+
+void print_pattern(){
+}
+
 void find_offset(char* prgrm){
-	printf("find offset received: %s\n", prgrm);
+
+	int child = fork();
+	
+	// CHILD
+	if (child == 0) {
+		char cmd[10000] = "./";
+		strcat(cmd, prgrm);
+		system(cmd);
+	}
+
+	// PARENT
+	else {
+		int status;
+		wait(&status);
+		while( !WIFEXITED(status) ){
+			wait(&status);
+		}
+		printf("program crashed!\n");
+	}
+	
 }
